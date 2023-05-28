72d854beb4c7ab5dd8bb999ec670d4ea5f5afc5a
VictorLMHT <134661700+VictorLMHT@users.noreply.github.com>
2023-05-28 14:38:22 +1000

    Update AS3.py

--- AS3.py
+++ AS3.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 conn = sqlite3.connect('Supershop.db')
 
 conn.execute('''CREATE TABLE IF NOT EXISTS product
@@ -9,12 +8,11 @@ conn.execute('''CREATE TABLE IF NOT EXISTS product
              quantity INT,
              price FLOAT)''')
 
-
 conn.execute("DELETE FROM product")
 
 product_data = [(10, "Jean", 50, 89.99),
-                  (20, "Shirt", 67, 59.99),
-                  (30, "Tshirt", 88, 39.99)]
+                (20, "Shirt", 67, 59.99),
+                (30, "Tshirt", 88, 39.99)]
 
 conn.executemany('INSERT INTO product (productID, productName, quantity, price) VALUES (?, ?, ?, ?)', product_data)
 
@@ -22,10 +20,11 @@ cursor = conn.execute("SELECT * FROM product")
 
 print("Welcome to the SuperShop Program!")
 
-options = ["\n"
-           "If you want to buy product, enter 1.",
-           "If you want to log as admin, enter 2.",
-           "If you want to quit, enter 0."]
+options = [
+    "\nIf you want to buy a product, enter 1.",
+    "If you want to log in as admin, enter 2.",
+    "If you want to quit, enter 0."
+]
 
 while True:
     print("\n".join(options))
@@ -38,14 +37,44 @@ while True:
         for row in cursor:
             print(str(row[0]) + ",\t   " + str(row[1]) + ",\t " + str(row[2]) + ",\t  " + str(row[3]))
 
+        order = {}  # Dictionary to store the selected products and quantities
 
-        print("\n Do you want to buy a product ?")
-        selection = input("Enter the product ID of your choice: ")
+        while True:
+            product_id = input("\nEnter the product ID of your choice (or 'done' to finish): ")
 
-        if selection == 10 or 20 or 30 :
-            print ("How many ?")
-#Boucle Jean
+            if product_id == "done":
+                break
 
+            # Check if the entered product ID exists in the database
+            cursor.execute("SELECT * FROM product WHERE productID = ?", (product_id,))
+            product = cursor.fetchone()
+
+            if product:
+                quantity = input("Enter the quantity for product ID {}: ".format(product_id))
+                order[product] = int(quantity)
+            else:
+                print("Invalid product ID entered. Try again.")
+
+        total_price = 0.0
+
+        print("\nOrdered Products:")
+        for product, quantity in order.items():
+            product_name = product[1]
+            product_price = product[3]
+            subtotal = product_price * quantity
+            total_price += subtotal
+            print("Product: {}, Quantity: {}, Subtotal: ${:.2f}".format(product_name, quantity, subtotal))
+
+        print("Total Price: ${:.2f}".format(total_price))
+
+        confirm = input("Do you want to confirm the order? (yes/no): ")
+
+        if confirm.lower() == "yes":
+            print("Order confirmed. Thank you!")
+        else:
+            print("Order cancelled.")
+
+#Loop Jean
 
     elif selection == "0":
         break
